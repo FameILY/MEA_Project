938a6edc9f5a1b3eca18034ba80de3dbf37f8e39
FameILY <bhambidsatyam@gmail.com>
2024-07-30 10:49:02 +0530

    apis member and contact us

---
+++ src/app/apis/route.js
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+import executeQuery from "../lib/db";
+
+
+export async function GET(req){
+    try {
+
+        const createTables = "CREATE TABLE IF NOT EXISTS memberReg (id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,fullName VARCHAR(45), email VARCHAR(45), contactNumber VARCHAR(20),designation VARCHAR(45), password VARCHAR(100), isApproved BOOLEAN DEFAULT FALSE); CREATE TABLE IF NOT EXISTS replies (replyId INT PRIMARY KEY AUTO_INCREMENT,fullName VARCHAR(45),email VARCHAR(45),contactNumber VARCHAR(20),address VARCHAR(200),message VARCHAR(300));"
+
+        const result = await executeQuery({
+            query: createTables
+        })
+        console.log(result)
+        return NextResponse.json({result: result})
+
+    } catch (err) {
+        console.log(err);
+    }
+
+} 
+
